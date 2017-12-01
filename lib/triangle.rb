3caaa0da940cd54b01e64699c4194ad2cc909b29
Araba Taylor <araba.taylor20@gmail.com>
2017-12-01 00:50:04 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ class Triangle
   end
 
   def validate_triangle
-    real_triangle = [(a + b > c), (a + c > b, (b + c > a))]
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
     if @a || @b || @c == 0
       raise TriangleError
     end
